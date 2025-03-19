4fc9c8318b2fcca2f0f922a72be23103955893d3
AmedMusl <124306572+AmedMusl@users.noreply.github.com>
2025-03-18 22:02:45 -0500

    Autotracking fixes

--- scripts/logic/logic.lua
+++ scripts/logic/logic.lua
@@ -13,3 +13,113 @@ function has_more_then_n_consumable(n)
     end
     return 0 -- 0 => no access
 end
+
+function has(item, amount)
+    local count = Tracker:ProviderCountForCode(item)
+    amount = tonumber(amount)
+    if not amount then
+        return count > 0
+    else
+        return count >= amount
+    end
+end
+
+function  coconut()
+    return has("donkey") and has("coconut")
+end
+
+function bongos()
+    return has("donkey") and has("bongos")
+end
+
+function grab()
+    return has("donkey") and has("grab")
+end
+
+function blast()
+    return has("donkey") and has("blast")
+end
+
+function strong()
+    return has("donkey") and has("strong")
+end
+
+function peanuts()
+    return has("diddy") and has("peanuts")
+end
+
+function guitar()
+    return has("diddy") and has("guitar")
+end
+
+function charge()
+    return has("diddy") and has("charge")
+end
+
+function spring()
+    return has("diddy") and has("spring")
+end
+
+function rocket()
+    return has("diddy") and has("rocket")
+end
+
+function grape()
+    return has("lanky") and has("grape")
+end
+
+function trombone()
+    return has("lanky") and has("trombone")
+end
+
+function ostand()
+    return has("lanky") and has("orangstand")
+end
+
+function balloon()
+    return has("lanky") and has("balloon")
+end
+
+function sprint()
+    return has("lanky") and has("sprint")
+end
+
+function feather()
+    return has("tiny") and has("feather")
+end
+
+function sax()
+    return has("tiny") and has("sax")
+end
+
+function twirl()
+    return has("tiny") and has("twirl")
+end
+
+function port()
+    return has("tiny") and has("port")
+end
+
+function mini()
+    return has("tiny") and has("mini")
+end
+
+function pineapple()
+    return has("chunky") and has("pineapple")
+end
+
+function triangle()
+    return has("chunky") and has("triangle")
+end
+
+function punch()
+    return has("chunky") and has("punch")
+end
+
+function gone()
+    return has("chunky") and has("gone")
+end
+
+function hunky()
+    return has("chunky") and has("big")
+end
